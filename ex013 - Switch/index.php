e0a0a5422403530e79567f73711a8df6885a7044
mateuslll <lima.mt96@gmail.com>
2023-12-11 14:39:48 +0000

    switch case

---
+++ ex013 - Switch/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    $tipo = 'foto';
+
+    switch ($tipo) {
+        case 'foto':
+            echo "exibindo foto";
+            break;
+        case 'video':
+            echo "exibindo video";
+            break;
+        case 'texto':
+            echo "exibindo texto";    
+            break;
+
+    }
+
+
+// o switch simplifica uma variavel em varias condiçoes diferentes, em um so bloco de codigo.
+
+?>
