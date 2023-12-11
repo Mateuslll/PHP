90b04c1d5b506637668b2190435bbfd1384ed50d
mateuslll <lima.mt96@gmail.com>
2023-12-11 14:17:04 +0000

    condicional ternario

---
+++ ex011 - condicional ternario/index.php
@@ -0,0 +1,19 @@
+<?php 
+    $idade = 17;
+
+if($idade >=18){
+    echo "Maior de Idade". $idade."<br/>";
+}else {
+   echo "Menor de Idade".$idade."<br/>";
+}
+
+//condicional ternaria é quando valida a propria variavel;
+
+echo ($idade>=18) ? "Maior de Idade" : "Menor de Idade";
+
+echo ($idade) ? true : false;
+
+
+
+
+?>
