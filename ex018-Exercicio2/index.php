1228a66e5873e5fa2374557278779bbdae1943ed
mateuslll <lima.mt96@gmail.com>
2023-12-16 20:44:01 -0300

    desafio loops2

---
+++ ex018-Exercicio2/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+$y=0;
+$quebradorLinha=1;
+for($i = 0; $i < 210; $i++){
+  echo "8";
+  $y++;
+  if($y >= $quebradorLinha){
+    echo "</br>";
+    $quebradorLinha++;
+    $y = 0;
+  }
+}
+ 
+
+
+
+/* 
+    -
+    --
+    ---
+    ---
+
+
+
+*/
+?>
+
+
+
+
